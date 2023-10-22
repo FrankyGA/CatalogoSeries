f18f9e26ff11df2c87b728ad6f63751f03e94b26
Fran <124530921+FrankyGA@users.noreply.github.com>
2023-10-23 00:34:44 +0200

    Act.23/10 cargar imagen desde base de datos, cambio de métodos y elminación de código innecesario

--- src/catalogoSeries/Controlador.java
+++ src/catalogoSeries/Controlador.java
@@ -1,125 +1,120 @@
 package catalogoSeries;
 
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class Controlador implements WindowListener, ActionListener {
-	
-	Modelo modelo;
-	VistaMenu vistaMenu;
-	VistaCatalogo vistaCatalogo;
-	
-	int serieActual = 1; //Inicializamos en la primera serie
-	
-	public Controlador(VistaMenu vistaMenu, Modelo modelo){
-		
-		this.vistaMenu = vistaMenu;
-		this.modelo = modelo;
-		//Agregar controladores
-		this.vistaMenu.addWindowListener(this);
-		this.vistaMenu.btnEntrar.addActionListener(this);
-			
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-	    if (vistaMenu != null && vistaMenu.isActive()) {
-	    	
-	        if (e.getSource().equals(vistaMenu.btnEntrar)) {
-	        	
-	            vistaMenu.setVisible(false); //Oculta VistaMenu
-	            vistaCatalogo = new VistaCatalogo();
-	            //Agregar controladores
-	            vistaCatalogo.addWindowListener(this);
-	            vistaCatalogo.btnNext.addActionListener(this);
-	            vistaCatalogo.btnBack.addActionListener(this);
-	            mostrarSerie(serieActual); //Mostrar la serie actual
-	            //Muestra la imagen de la primera serie al acceder al catálogo
-	            //String imagenPrimera = "img/" + serieActual + ".jpg";
-	            //vistaCatalogo.actualizarImagenPortada(imagenPrimera);
-	            /*String imagePath = modelo.obtenerPortadaSerie1();
-	            //vistaCatalogo.actualizarImagenPortada(imagePath);*/
-	          
-	            vistaCatalogo.actualizarImagenPortada("img/1.jpg");
-	            System.out.println("Clicked catalogo");//Para comprobar funcionalidad correcta del botón
-	        } 
-	    } 
-	    //Condicion para moverte en el catálogo
-	    else if (vistaCatalogo != null && vistaCatalogo.isActive()) {
-	    	//Si pulsamos en el botón Next accede a la siguiente serie del catálogo
-	        if (e.getSource().equals(vistaCatalogo.btnNext)) {
-	        	
-	            serieActual++;
-	            mostrarSerie(serieActual);
-	      
-	        } 
-	        //Si pulsamos en el botón Back accede a la serie anterior del catálogo
-	        else if (e.getSource().equals(vistaCatalogo.btnBack)) {
-	        	
-	            serieActual--;
-	            mostrarSerie(serieActual);
-	            
-	        }
-	    }
-	}
-	//Método privado de controlador para insertar los datos de la serie que se va a mostrar en la vista
-	//Usa el método del modelo para sacar los datos de la base de datos
-    private void mostrarSerie(int id) {
-        String serieTexto = modelo.mostrarSerieDatos(id);
 
-        //Actualiza la imagen de la portada
-        String imagePath = "img/" + id + ".jpg";
-        vistaCatalogo.actualizarImagenPortada(imagePath);
-            
+    Modelo modelo;
+    VistaMenu vistaMenu;
+    VistaCatalogo vistaCatalogo;
+    //Iniciamos la serie actual al entrar en el catálogo
+    int serieActual = 1;
+
+    public Controlador(VistaMenu vistaMenu, Modelo modelo) {
+        this.vistaMenu = vistaMenu;
+        this.modelo = modelo;
+        this.vistaMenu.addWindowListener(this);
+        this.vistaMenu.btnEntrar.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (vistaMenu != null && vistaMenu.isActive()) {
+            if (e.getSource().equals(vistaMenu.btnEntrar)) {
+            	//Al pulsar en el bóton entrar en el menú principal accedemos a la vista del catálogo
+                vistaMenu.setVisible(false);
+                vistaCatalogo = new VistaCatalogo();
+                vistaCatalogo.addWindowListener(this);
+                vistaCatalogo.btnNext.addActionListener(this);
+                vistaCatalogo.btnBack.addActionListener(this);
+                mostrarSerie(serieActual);
+            }
+        } else if (vistaCatalogo != null && vistaCatalogo.isActive()) {
+        	//Control de botones para movernos por el catálogo
+            if (e.getSource().equals(vistaCatalogo.btnNext)) {
+                serieActual++;
+                if (!mostrarSerie(serieActual)) {
+                    vistaCatalogo.txaDescripcion.setText("No hay más registros.");
+                    vistaCatalogo.btnNext.setEnabled(false);
+                    vistaCatalogo.btnBack.setEnabled(true);
+                }
+            } else if (e.getSource().equals(vistaCatalogo.btnBack)) {
+                serieActual--;
+                mostrarSerie(serieActual);
+                vistaCatalogo.btnNext.setEnabled(true);
+                vistaCatalogo.btnBack.setEnabled(true);
+                if(serieActual==1) {
+                	vistaCatalogo.btnBack.setEnabled(false);
+                }
+            }
+        }
+    }
+    /*Método para mostrar las series en el catálogo
+    Usa los métodos del Modelo para mostrar los datos y la portada de cada serie y que estan guardados en
+    la base de datos*/
+    private boolean mostrarSerie(int id) {
+        String serieTexto = modelo.mostrarSerieDatos(id);
+        String imagePath = modelo.obtenerRutaImagen(id);
+
         if (serieTexto != null) {
             vistaCatalogo.txaDescripcion.setText(serieTexto);
 
-            // Verifica si es la primera o última serie para controlar el catálogo
-            if (serieActual == 1) {
-                vistaCatalogo.txaDescripcion.append("\n\nEsta es la primera serie.");
-            } else {
-                String siguienteSerie = modelo.mostrarSerieDatos(id + 1);
-                if (siguienteSerie == null) {
-                    vistaCatalogo.txaDescripcion.append("\n\nEsta es la última serie.");
+            if (imagePath != null) {
+                Image imagenPortada = Toolkit.getDefaultToolkit().getImage(imagePath);
+                MediaTracker mediaTracker = new MediaTracker(vistaCatalogo);
+                mediaTracker.addImage(imagenPortada, 0);
+
+                try {
+                    mediaTracker.waitForAll();
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
                 }
+
+                vistaCatalogo.actualizarImagenPortada(imagenPortada);
+            } else {
+                vistaCatalogo.actualizarImagenPortada(null); // Puedes establecer una imagen por defecto
             }
 
-            
+            if (id == 1) {
+                vistaCatalogo.btnBack.setEnabled(false);
+            }
         }
+        return serieTexto != null;
     }
-	@Override
-	public void windowOpened(WindowEvent e){}
-	@Override
-	/*public void windowClosing(WindowEvent e) {
-        if (vistaMenu != null && vistaMenu.isActive()) {
+
+    @Override
+    public void windowOpened(WindowEvent e) {}
+
+    @Override
+    //Añade funcionalidad a las ventanas para cerrarlas
+    public void windowClosing(WindowEvent e) {
+        if (e.getSource() instanceof VistaMenu) {
             vistaMenu.setVisible(false);
-        } else if (vistaCatalogo != null && vistaCatalogo.isActive()) {
+        } else if (e.getSource() instanceof VistaCatalogo) {
             vistaCatalogo.setVisible(false);
         } else {
             System.exit(0);
         }
-    }*/
-	//instanceof para verificar si se genero el evento
-	public void windowClosing(WindowEvent e) {
-	    if (e.getSource() instanceof VistaMenu) {
-	        vistaMenu.setVisible(false);
-	    } else if (e.getSource() instanceof VistaCatalogo) {
-	        vistaCatalogo.setVisible(false);
-	    } else {
-	        System.exit(0);
-	    }
-	}
+    }
 
-	@Override
-	public void windowClosed(WindowEvent e) {}
-	@Override
-	public void windowIconified(WindowEvent e) {}
-	@Override
-	public void windowDeiconified(WindowEvent e) {}
-	@Override
-	public void windowActivated(WindowEvent e) {}
-	@Override
-	public void windowDeactivated(WindowEvent e) {}
+    @Override
+    public void windowClosed(WindowEvent e) {}
+
+    @Override
+    public void windowIconified(WindowEvent e) {}
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {}
+
+    @Override
+    public void windowActivated(WindowEvent e) {}
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {}
 }
--- src/catalogoSeries/Modelo.java
+++ src/catalogoSeries/Modelo.java
@@ -8,70 +8,81 @@ import java.sql.SQLException;
 
 public class Modelo {
 
-    String driver = "com.mysql.cj.jdbc.Driver";
-    String url = "jdbc:mysql://localhost:3306/catalogoSeries";
-    String login = "userSeries"; //Usuario
-    String password = "Studium2023;"; //Clave de acceso
-    //String sentencia = "";
-    Connection connection = null;//Objeto de conexion a BD
+	String driver = "com.mysql.cj.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/catalogoSeries";
+	String login = "userSeries"; // Usuario
+	String password = "Studium2023;"; // Clave de acceso
+	Connection connection = null;
 
-    public Modelo() {
-        connection = this.conexion();
-    }
+	public Modelo() {
+		connection = this.conexion();
+	}
 
-    //Método para conectar con la base datos
-    private Connection conexion() {
-        try {
-            // Cargar los controladores para el acceso a la BD
-            Class.forName(driver);
-            // Establecer la conexión con la BD
-            return DriverManager.getConnection(url, login, password);
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        return null;
-    }
-  //Método para desconectar con la base datos
-    public void desconectar() {
-        try {
-            if (connection != null) {
-                connection.close();
-            }
-        } catch (SQLException e) {}
-    }
+	//Método para establecer una conexión a la base de datos
+	private Connection conexion() {
+		try {
+			Class.forName(driver);
+			return DriverManager.getConnection(url, login, password);
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		return null;
+	}
 
-    //Método para sacar datos de la BD, se pasa por parámetros el id para sacar uno a uno los registros
-    public String mostrarSerieDatos(int id) {
-        try {
-        	//Hacemos consulta a la base de datos segun el ID para diferenciar entre series
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM series WHERE idSerie = ?");
-            statement.setInt(1, id);
-            ResultSet resultSet = statement.executeQuery();
+	//Método para desconectar la base de datos
+	public void desconectar() {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+		}
+	}
 
-            //Creamos variables para guardar datos uno a uno
-            if (resultSet.next()) {
-            	//Guardamos los datos de la BD
-                String titulo = resultSet.getString("titulo");
-                String director = resultSet.getString("director");
-                String actores = resultSet.getString("actores");
-                String genero = resultSet.getString("genero");
-                int temporadas = resultSet.getInt("temporadas");
-                int anyo = resultSet.getInt("anyo");
+	//Método para obtener los datos de los registros de cada serie
+	public String mostrarSerieDatos(int id) {
+		try {
+			//Consulta SQL para obtener los datos de una serie por su ID
+			PreparedStatement statement = connection.prepareStatement("SELECT * FROM series WHERE idSerie = ?");
+			statement.setInt(1, id);
+			ResultSet resultSet = statement.executeQuery();
 
-                //Crear un formato de texto para mostrar la información en un TextArea
-                String serieTexto = "Título: " + titulo + "\nDirector: " + director + "\nActores: " + actores
-                        + "\nGénero: " + genero + "\nTemporadas: " + temporadas + "\nAño: " + anyo;
-                return serieTexto;//Devuelve el texto formateado para el text area
-            }
-        } catch (SQLException ex) {//Controla errores
-            ex.printStackTrace();
-        }
-        return null;//Devuelve nulo sino se encontraron los datos
-    }
+			if (resultSet.next()) {
+				//Obtener los datos de la serie desde el resultado de la consulta
+				String titulo = resultSet.getString("titulo");
+				String director = resultSet.getString("director");
+				String actores = resultSet.getString("actores");
+				String genero = resultSet.getString("genero");
+				int temporadas = resultSet.getInt("temporadas");
+				int anyo = resultSet.getInt("anyo");
 
-    
-	/*public String obtenerPortadaSerie1() {
-		// TODO Auto-generated method stub
-		return "img/1.jpg";
-	}*/
+				//Formateo para los datos de la serie y mostrarlos en el text area
+				String serieTexto = "Título: " + titulo + "\nDirector: " + director + "\nActores: " + actores
+						+ "\nGénero: " + genero + "\nTemporadas: " + temporadas + "\nAño: " + anyo;
+				return serieTexto;
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		//Devuelve nulo si no se encontraron datos de la serie
+		return null;
+	}
+
+	//Método para obtener la ruta de la imagen desde la base de datos
+	public String obtenerRutaImagen(int id) {
+		try {
+			PreparedStatement statement = connection.prepareStatement("SELECT imagen FROM series WHERE idSerie = ?");
+			statement.setInt(1, id);
+			ResultSet resultSet = statement.executeQuery();
+
+			if (resultSet.next()) {
+				//Obtener la ruta de la imagen desde el resultado de la consulta
+				return resultSet.getString("imagen");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		//Devuelve nulo si no se encontró la ruta de la imagen
+		return null;
+	}
 }
